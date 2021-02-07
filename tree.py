e6dd38608151055c9c4e7d1c2e34f05774fba1f3
Douglas De León <dele18037@uvg.edu.gt>
2021-02-07 16:00:42 -0600

    Tree node implementation

---
+++ tree.py
@@ -0,0 +1,10 @@
+"""
+Each reached states can be sotred s a lookup table (dict) where each key is a state and each value is the node for that state. 
+"""
+
+class Node:
+    def __init__(self, state, parent, action, value) -> None:
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.value = value
